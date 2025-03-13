79d5a2fe8198dbf8c184a159ab4c936e08f549fa
arcmee <kihyun3297@gmail.com>
2025-03-13 10:00:15 +0900

    Quiz 3

---
+++ bin/Quiz3.dart
@@ -0,0 +1,33 @@
+
+void main(){
+  print(longestCommonPrefix(["flower","flow","flight"]));
+}
+
+String longestCommonPrefix(List<String> strs) {
+  List<List<String>> strList = [];
+
+  for(String str in strs){
+    List<String> temp = [];
+    for(int i = 0; i < str.length ; ++i){
+      temp.add(str[i]);
+    }
+    strList.add(temp);
+  }
+  String retStr = '';
+
+  List<String> first = strList[0];
+  int curIndex = 0;
+  bool matched = false;
+
+  for(String s in first){
+    for(int i = 1; i < strList.length ; ++i){
+      if(curIndex > strList[i].length) break;
+      matched = strList[i][curIndex] == first[curIndex];    
+    }
+    if(matched){
+      retStr += first[curIndex++];
+    }
+  }
+
+  return retStr;
+}
